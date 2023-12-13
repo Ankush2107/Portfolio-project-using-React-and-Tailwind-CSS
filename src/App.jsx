df78f7aa6bfbd553c849a9c5dad51f6a2b77df6c
Ankush2107 <ankush.kumar210701@gmail.com>
2023-12-13 10:39:48 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import About from './pages/About/About';
+function App() {
+
+  return (
+    <div className="App">
+        <Router>
+            <Routes>
+                <Route path='' element={<Home/>} />
+                <Route path='about' element={<About/>} />
+            </Routes>
+        </Router>
+    </div>
+  )
+}
+
+export default App
